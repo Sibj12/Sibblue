66fdc7688acc678b68406891858865bc054c69d0
Sibyl Jean <Sibyljean86@gmail.com>
2023-06-01 00:13:33 +0000

    Done

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,8 @@ print('I\'m learning to become a Python developer !\nI\'m so excited!')
 print ('What is your name?')
 user_name = input ()
 print('Hello,', user_name)
+
+login = input('Enter your login: ')
+language = input('Enter your native language: ')
+ 
+print('Your login is', login, 'and you speak', language)
