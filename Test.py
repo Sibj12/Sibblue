871ed385478b1a86f786222438e6b617c21aa158
Sibyl Jean <Sibyljean86@gmail.com>
2023-06-04 03:41:40 +0000

    test

--- Test.py
+++ Test.py
@@ -17,5 +17,3 @@ sum = float(num1) + float(num2)
 
 #Display the sum
 print('The sum of {0} and {1} is {2}' .format(num1, num2, sum))
-
-print('The sum is %. if' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
