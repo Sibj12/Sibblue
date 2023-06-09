31da4118b6e9fafe225971853ae62063c6174221
Sibyl Jean <Sibyljean86@gmail.com>
2023-06-09 00:17:29 +0000

    Enter

---
+++ BEGINNER.py
@@ -0,0 +1,9 @@
+# dog years converter
+#input dog years
+#output human years
+
+def dog_year_converter(years):
+    #human years multiplied by 7
+    return years*7
+    
+print(dog_year_converter(2))
